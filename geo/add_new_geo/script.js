c37c1c9d5a0bab787d604e83f54e8ad99eefb084
U-ISP\v.sadovnikov <v.sadovnikov@WS-081.isp.local>
2019-03-18 16:47:13 +0300

    [master] modif .gitignore

---
+++ geo/add_new_geo/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by v.sadovnikov on 18.03.2019.
+ */
--- sender/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function check_str ($str, $check_words){
-    if(stristr($str, $check_words) == TRUE) return TRUE;
-    else return FALSE;
-}
-
-
