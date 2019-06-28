96a1d377610c75f7a1485ca8199744bf0473fb25
v.sadovnikov <v.sadovnikov@cityads.team>
2019-06-28 17:24:25 +0300

    fix bug with import in csv

--- ali_orders/get_data.php
+++ ali_orders/get_data.php
@@ -2,58 +2,56 @@
 include_once '../Classes/func.php';
 download_send_headers("ali_data_export_" . date("Y-m-d H:i:s") . ".csv");
 
-if (!empty($_POST['data'])){
+function fetchOrdersFromApiAlibaba($orders) {
+    $ordersStr = '';
+    for ($i = 0; $i < count($orders); $i++){
+        
+        $ordersStr .= ','.$orders[$i];
+    }
+    $ordersStr = substr($ordersStr, 1);
+    $url = "https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getOrderStatus/30056?appSignature=9FIO77dDIidM&orderNumbers=". $ordersStr;
+    
+    $result =  curl($url);
+    $result = json_decode($result, TRUE);
+    $orders = $result['result']['orders'];
+    $sortOrders = [];
+    foreach ($orders as $order) {
+        $sortOrders[] = fixAlibabaDataFormat($order);
+    }
+    return $sortOrders;
+}
 
-        $data = $_POST['data'];
-        $data = explode(',', $data);
-        $result = '';
+function fixAlibabaDataFormat($order) {
+    if (!isset($order['transactionTime'])) {
+        $order['transactionTime'] = '';
+    }
+    ksort($order);
+    return $order;
+}
+
+if (!empty($_POST['data'])){
+        $arrayResponseFromApi = [];
+        $arrayResult = [];
+        $orders = $_POST['data'];
+        $orders = explode(',', $orders);
 
         // разделяем 2 сценария , если свыше 100 ордеров и наоборот
-        if (count($data) > 100){
+        if (count($orders) > 100){
 
             // дробим по 100 ордеров и отправляем в алибабу
-            $data = array_chunk($data, 100);
-
-            for ($i = 0; $i < count($data); $i++){
-
-                $str = '';
-
-                for ($n = 0; $n < count($data[$i]); $n++){
-                    $str .= ','.$data[$i][$n];
-                }
-                $str = substr($str, 1);
-                $url = "https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getOrderStatus/30056?appSignature=9FIO77dDIidM&orderNumbers=". $str;
-
-             $responses =  curl($url);
-             // ответ алибабы преобразуем в массив
-             $responseArr[] = json_decode($responses, TRUE);
-
-
+            $bigOrders = array_chunk($orders, 100);
+            
+            foreach ($bigOrders as $ordersChunk) {
+                $arrayResponseFromApi[] = fetchOrdersFromApiAlibaba($ordersChunk);
             }
-
-            $array = [];
-
-            // преобразуем массив и склеиваем чанки
-            foreach ($responseArr as $response){
-                $array[]= $response['result']['orders'];
-            }
-            $array = array_merge(...$array); // отличное решение, элементы массива = подмассивы через оператор ... встраиваются в функцию мерж
+            
+            $arrayResult = array_merge(...$arrayResponseFromApi); // отличное решение, элементы массива = подмассивы через оператор ... встраиваются в функцию мерж
 
         } else{
-            $str = '';
-            for ($i = 0; $i < count($data); $i++){
-
-                $str .= ','.$data[$i];
-            }
-            $str = substr($str, 1);
-            $url = "https://gw.api.alibaba.com/openapi/param2/2/portals.open/api.getOrderStatus/30056?appSignature=9FIO77dDIidM&orderNumbers=". $str;
-
-            $result =  curl($url);
-            $result = json_decode($result, TRUE);
-            $array = $result['result']['orders'];
+            $arrayResult = fetchOrdersFromApiAlibaba($orders);
         }
-
+        
         // преобразуем массив в цсв
-    echo array2csv($array);
+    echo array2csv($arrayResult);
     }
 ?>
