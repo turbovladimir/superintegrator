458fe182d9496ffb8eaf768cb42d4311d6d1b14d
v.sadovnikov <v.sadovnikov@cityads.team>
2019-07-19 11:14:25 +0300

    add daemon feature

--- sender/db2server.php
+++ sender/db2server.php
@@ -2,51 +2,63 @@
 include '../autoload.php';
 include_once '../config.php';
 
+const SLEEP_TIME_SECONDS = 30;
+$cycles = 20;
+
+while (true) {
+
+    if ($cycles === 0) {
+        break;
+    }
+    
 // подключаемся к бд и забираем данные
-try {
-    $db = new simpleQuery($connectParams);
-    $urls = $db->selectColumnFromTable($tablePostbacks, 'url', 1000);
-
-    function CheckOkResponse($responseStr, $info, RollingCurlRequest $request)
-    {
-        global $responseOK;
-        if ($info['http_code'] === 200){
-            $responseOK[] = $request->url;
-        }
-    }
-
-    // отправляем реквесты если они есть:
-    if (!empty($urls)){
-        $AC = new AngryCurl('CheckOkResponse');
-
-        foreach ($urls as $url)
+    try {
+        $db   = new simpleQuery($connectParams);
+        $urls = $db->selectColumnFromTable($tablePostbacks, 'url', 1000);
+        
+        function CheckOkResponse($responseStr, $info, RollingCurlRequest $request)
         {
-            // adding URL to queue
-            $AC->get($url, $headers = null, $options = null);
-
+            global $responseOK;
+            if ($info['http_code'] === 200) {
+                $responseOK[] = $request->url;
+            }
         }
-
-        // setting amount of threads and starting connections
-        $AC->execute(200);
-
-        unset($AC);
-
-
-        // проставляем флаги успешной отправки в таблицу с постбэками
-        $counter = count($responseOK);
-
-        // проверяем коннект
-        if (!($db->checkConnect())){
-            $db = new simpleQuery($connectParams);
+        
+        // отправляем реквесты если они есть:
+        if (!empty($urls)) {
+            $AC = new AngryCurl('CheckOkResponse');
+            
+            foreach ($urls as $url) {
+                // adding URL to queue
+                $AC->get($url, $headers = null, $options = null);
+                
+            }
+            
+            // setting amount of threads and starting connections
+            $AC->execute(200);
+            
+            unset($AC);
+            
+            
+            // проставляем флаги успешной отправки в таблицу с постбэками
+            $counter = count($responseOK);
+            
+            // проверяем коннект
+            if (!($db->checkConnect())) {
+                $db = new simpleQuery($connectParams);
+            }
+            
+            for ($i = 0; $i < $counter; $i++) {
+                $db->updateCellInTable($tablePostbacks, 'url', $responseOK[$i], 'sended', '1'); // обновили
+            }
         }
-
-        for ($i = 0; $i < $counter; $i++){
-               $db->updateCellInTable($tablePostbacks,'url', $responseOK[$i], 'sended', '1'); // обновили
-        }
+    } catch (dataBaseException $ex) {
+        //Выводим сообщение об исключении.
+        echo $ex->getMessage();
     }
-}catch (dataBaseException $ex) {
-    //Выводим сообщение об исключении.
-    echo $ex->getMessage();
+    
+    sleep(SLEEP_TIME_SECONDS);
+    $cycles --;
 }
 # Рабочий крон
 # %progdir%\modules\wget\bin\wget.exe -q --no-cache http://test/city_dooDOS/send_to_city_from_db.php
