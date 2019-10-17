5601adab714dc2ac84a98d25d10923d3e939e98b
v.sadovnikov <v.sadovnikov@cityads.team>
2019-10-17 16:00:43 +0300

    add id in csv entity

--- src/Entity/CsvEntity.php
+++ src/Entity/CsvEntity.php
@@ -18,11 +18,19 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class CsvEntity
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
     
     /**
      * @var int
      *
-     * @ORM\Column(name="file_name", type="string", ength=40 , nullable=false)
+     * @ORM\Column(name="file_name", type="string", length=40 , nullable=false)
      */
     private $filename;
     
