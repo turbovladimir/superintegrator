b4bbdfe2bdadcca802ac2d798510db7aebf8ec18
v.sadovnikov <v.sadovnikov@cityads.team>
2020-12-11 12:03:22 +0300

    migration add

--- src/Migrations/Version20200720115138.php
+++ src/Migrations/Version20200720115138.php
@@ -21,8 +21,6 @@ final class Version20200720115138 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE test_xml CHANGE url hash VARCHAR(80) NOT NULL');
     }
 
@@ -30,8 +28,6 @@ final class Version20200720115138 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, email VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, plain_password VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, roles JSON NOT NULL COMMENT \'(DC2Type:json_array)\', added_at DATETIME NOT NULL, confirmation_code VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, enabled TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE test_xml CHANGE hash url VARCHAR(80) CHARACTER SET utf8 NOT NULL COLLATE `utf8_general_ci`');
     }
 }
